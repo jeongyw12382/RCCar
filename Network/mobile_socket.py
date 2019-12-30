086ee619e27d3bc070ccdad9ca2822b53542055b
jeongyw12382 <jeongyw12382@postech.ac.kr>
2019-12-30 01:14:56 -0800

    Checking POSLA interface works well

--- Network/mobile_socket.py
+++ Network/mobile_socket.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+
 PORT = 10001
 
 
@@ -24,12 +25,17 @@ if __name__ == '__main__':
     # To send the message on server
     # Information about current location,
     send_sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    send_sck.connect(('127.0.0.1', PORT))
+    send_sck.connect(('141.223.162.232', PORT))
+    camera = Camera.Camera(send_sck)
     send_thread = threading.Thread(target=send_socket, args=(send_sck, ))
     get_thread = threading.Thread(target=listen_socket, args=(send_sck, ))
+    camera_thread = threading.Thread(target=camera.run, args=())
+    camera_thread.start()
+
     send_thread.start()
     get_thread.start()
 
     # Wait until both threads are terminated. Then close.
     send_thread.join()
     get_thread.join()
+    camera_thread.join()
